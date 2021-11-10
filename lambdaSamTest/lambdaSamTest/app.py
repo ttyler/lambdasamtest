1452f7f34b3c7b6a615e3aa009f7a5d6fb28fef2
Tomos Tyler <tomos.tyler@gmail.com>
2021-11-10 15:54:47 +0700

    CleanUp

--- lambdaSamTest/lambdaSamTest/app.py
+++
@@ -1,8 +0,0 @@
-
-def lambda_handler(event, context):
-    message = 'Hello {} {}!'.format(event['obj_name'], 
-                                    event['body'])  
-    return { 
-        'message' : message
-    }  
-    
